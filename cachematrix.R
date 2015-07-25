e4b80c06bc4b6100b55904ca737217a269ecfa5d
lacelofranco <lace.lofranco@gmail.com>
2015-07-26 00:10:13 +1000

    Programming solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,73 @@
-## Put comments here that give an overall description of what your
-## functions do
+# R Programming assignment 2
 
-## Write a short comment describing this function
 
+# This function allows caching of the matrix inverse as a function variable 
+# returning a list (acting like a pseudo object) with getters and setters
+#
+# INPUT PARAMETERS: 
+# matrix (optional - if not provided an empty matrix is created)
+#
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  matrixInverse <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    matrixInverse <<- NULL
+  }
+  
+  get <- function() {
+    x
+  }
+  
+  setMatrixInverse <- function(inverse) {
+    matrixInverse <<- inverse
+  } 
+  
+  getMatrixInverse <- function() {
+    matrixInverse
+  }
+  
+  list(set = set, get = get,
+       setMatrixInverse = setMatrixInverse,
+       getMatrixInverse = getMatrixInverse)
 }
 
 
-## Write a short comment describing this function
-
+# This function returns a matrix that is the inverse of 'x', taking in output of makeCacheMatrix
+# It initially checks if a cached inverse matrix exists prior to computing the inverse
+#
+# INPUT PARAMETERS: 
+# matrix (optional - if not provided an empty matrix is created)
+#
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  matrixInverse <- x$getMatrixInverse()
+  
+  if(!is.null(matrixInverse)) {
+    message("getting cached matrix inverse")
+    return(matrixInverse)
+  }
+  
+  data <- x$get()
+  matrixInverse <- solve(data)
+  x$setMatrixInverse(matrixInverse)
+  
+  matrixInverse
+        
 }
+
+########################
+# Sample use
+
+# Create the sample cached matrix
+sampleCachedMatrix = makeCacheMatrix(matrix(c(4,3,3,2), nrow = 2, ncol = 2, byrow = TRUE))
+
+# Inital call, so matrix is not yet cached
+cacheSolve(sampleCachedMatrix) 
+
+# Second call, matrix cached is retrieved. Notice "getting cached matrix inverse" is printed out.
+cacheSolve(sampleCachedMatrix) 
+
+
+
